60bd2a0edb463f455746277fc20253404a046277
Briojas <blake.a.riojas@gmail.com>
2023-12-20 12:03:41 -0600

    part 2 passing

--- Advent_of_Code/2023/day_01/src/bin/part2.rs
+++ Advent_of_Code/2023/day_01/src/bin/part2.rs
@@ -8,9 +8,31 @@ pub fn part2(input: &str) -> String {
     let output = input
         .lines()
         .map(|line|{
-            let mut it =
-                line.chars().filter_map(|character|{
-                    character.to_digit(10)
+            let mut it = (0..line.len())
+                .filter_map(|index|{
+                    let reducedLine = &line[index..];
+                    let result = if reducedLine.starts_with("one"){
+                        '1'
+                    } else if reducedLine.starts_with("two"){
+                        '2'
+                    } else if reducedLine.starts_with("three"){
+                        '3'
+                    } else if reducedLine.starts_with("four"){
+                        '4'
+                    } else if reducedLine.starts_with("five"){
+                        '5'
+                    } else if reducedLine.starts_with("six"){
+                        '6'
+                    } else if reducedLine.starts_with("seven"){
+                        '7'
+                    } else if reducedLine.starts_with("eight"){
+                        '8'
+                    } else if reducedLine.starts_with("nine"){
+                        '9'
+                    } else {
+                        reducedLine.chars().next().unwrap()
+                    };
+                    result.to_digit(10)
                 });
             let first = 
                 it.next().expect("should be a number");
