3ac200c45357b38ea3152fc753e4eb44ea465fd5
Briojas <blake.a.riojas@gmail.com>
2023-12-20 11:32:13 -0600

    part 1 complete, setting up part 2

--- Advent_of_Code/2023/day_01/src/bin/part2.rs
+++ Advent_of_Code/2023/day_01/src/bin/part2.rs
@@ -5,7 +5,25 @@ fn main() {
 }
 
 pub fn part2(input: &str) -> String {
-    "todo!()".to_string()
+    let output = input
+        .lines()
+        .map(|line|{
+            let mut it =
+                line.chars().filter_map(|character|{
+                    character.to_digit(10)
+                });
+            let first = 
+                it.next().expect("should be a number");
+            let last = it.last();
+            match last {
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
+            }
+            .parse::<u32>()
+            .expect("should be a number")
+    })
+    .sum::<u32>();
+    output.to_string()
 }
 
 #[cfg(test)]
@@ -14,7 +32,13 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let result = part2("");
-        assert_eq!(result, "0".to_string());
+        let result = part2("two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen");
+        assert_eq!(result, "281".to_string());
     }
 }
