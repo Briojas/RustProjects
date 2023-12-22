c1fa98739d753a23f7850f93def3a1f2f38565c8
Briojas <blake.a.riojas@gmail.com>
2023-12-22 09:28:01 -0600

    first attempt failed

--- Advent_of_Code/2023/day_02/src/bin/part1.rs
+++ Advent_of_Code/2023/day_02/src/bin/part1.rs
@@ -5,8 +5,42 @@ fn main() {
 }
 
 pub fn part1(input: &str) -> String {
-    let output = "TODO";
-    output.to_string()
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
+
+    let mut total = 0;
+    let mut id = 0;
+    let mut value = 0;
+
+    let _output = input
+        .lines()
+        .map(|line|{
+            for word in line.split_whitespace() {
+                if word.contains(":") {
+                    id = word[0..1].parse::<i32>().unwrap();
+                } else if word.parse::<i32>().is_ok() {
+                    value = word.parse::<i32>().unwrap();
+                } else if word == "blue," {
+                    if value > max_blue {
+                        id = 0;
+                        break;
+                    }
+                } else if word == "green," {
+                    if value > max_green {
+                        id = 0;
+                        break;
+                    }
+                } else if word == "red," {
+                    if value > max_red {
+                        id = 0;
+                        break;
+                    }
+                }
+            }
+            total = total + id;
+        });
+    total.to_string()
 }
 
 #[cfg(test)]
@@ -15,10 +49,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let result = part1("1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet");
-        assert_eq!(result, "142".to_string());
+        let result = part1("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
+        assert_eq!(result, "8".to_string());
     }
 }
