41fdd96aafea0e26ad519016ec3949ff9ac47e2e
Briojas <blake.a.riojas@gmail.com>
2025-03-05 12:07:07 -0600

    part1 soln w/ trimmings

--- Advent_of_Code/2023/day_02/src/bin/part2.rs
+++ Advent_of_Code/2023/day_02/src/bin/part2.rs
@@ -5,8 +5,42 @@ fn main() {
 }
 
 pub fn part2(_input: &str) -> String {
-    let output = "TODO";
-    output.to_string()
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
+
+    let mut result = 0;
+
+    let _output = _input
+        .lines()
+        .for_each(|line|{
+            let mut game = line.split(": ");
+            let _game_id = game.next().unwrap();
+            let game_id_number = _game_id.split_whitespace().nth(1).unwrap().parse::<i32>().unwrap();
+            let game = game.next().unwrap();
+            let colors = game.split("; ");
+            let mut red_highest = 0;
+            let mut green_highest = 0;
+            let mut blue_highest = 0;
+            for color in colors {
+                let color = color.split(", ");
+                for c in color {
+                    let mut c = c.split_whitespace();
+                    let count = c.next().unwrap().parse::<i32>().unwrap();
+                    let color = c.next().unwrap();
+                    match color {
+                        "red" => if count > red_highest { red_highest = count },
+                        "green" => if count > green_highest { green_highest = count },
+                        "blue" => if count > blue_highest { blue_highest = count },
+                        _ => (),
+                    }
+                }
+            }
+            if red_highest <= max_red && green_highest <= max_green && blue_highest <= max_blue {
+                result += game_id_number;
+            }
+        });
+    result.to_string()
 }
 
 #[cfg(test)]
